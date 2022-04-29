f62afe69cc2704c4875477314995a61f4ef951da
sougabioliveira <oliveira.gabrielagomes@gmail.com>
2022-04-29 15:27:52 -0300

    Adicionando atividade do dia 29/04/2022

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -3,9 +3,15 @@ package com.generation.blogpessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,19 +20,51 @@ import com.generation.blogpessoal.repository.PostagemRepository;
 
 @RestController
 @RequestMapping("/postagens")
-// O '*' quer dizer que essa classe aceita requisições de qualquer origem
+// Quer dizer que essa classe aceita requisições de qualquer origem
 @CrossOrigin("*")
 public class PostagemController {
 
 	//garantimos que todos os serviços da interface seja acessada a partir do controller
 	@Autowired
-	public PostagemRepository repository;
+	private PostagemRepository repository;
 
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){
 		return ResponseEntity.ok(repository.findAll());
 	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> GetById(@PathVariable long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
 	
-}
+	/* este código precisa ser rodado contendo elementos do seu próprio titulo
+	*/
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	//para inserir dados novos
+	 @PostMapping
+	 public ResponseEntity<Postagem> post (@RequestBody Postagem postagem) {
+		 return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	 }
+	
+	 //faz a atualização dos dados
+	 @PutMapping
+	 public ResponseEntity<Postagem> put (@RequestBody Postagem postagem) {
+		 return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	 }
+	 
+	 // metodo para deletar dados
+	 @DeleteMapping("/{id}")
+	 public void delete(@PathVariable long id) {
+		 //este não vai usar return, pois é do tipo void e não retorna nada
+		 repository.deleteById(id);
+	 }
+	 
+	}
+
 	
 
